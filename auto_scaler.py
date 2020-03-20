390de6e8e54927427d5af3ecea2668ed5cbdc0d5
Sanchit Pruthi <sanchitpruthi@gmail.com>
2020-03-20 15:41:06 -0700

    Added AS - SP

--- auto_scaler.py
+++ auto_scaler.py
@@ -16,20 +16,20 @@ def process_ec2_machines():
   client = boto3.client('sqs')
   queue = sqs.get_queue_by_name(QueueName='video-key')
 
-    que=queue.receive_messages(MessageAttributeNames=['Video-Key'])
-    que_length=int(queue.attributes.get('ApproximateNumberOfMessages'))
-  
-    if que_length > 0:
-      # Get list of stopped and running instances
-      ec2 = boto3.resource('ec2')
-      running_instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-      stopped_instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
-      
-      for inst in running_instances:
-        print("Running: ", inst.id, instance.instance_type)
-      
-      for inst in stopped_instances:
-        print("Stopped: ", inst.id, instance.instance_type)
+  que=queue.receive_messages(MessageAttributeNames=['Video-Key'])
+  que_length=int(queue.attributes.get('ApproximateNumberOfMessages'))
+
+  if que_length > 0:
+    # Get list of stopped and running instances
+    ec2 = boto3.resource('ec2')
+    running_instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    stopped_instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+    
+    for inst in running_instances:
+      print("Running: ", inst.id, instance.instance_type)
+    
+    for inst in stopped_instances:
+      print("Stopped: ", inst.id, instance.instance_type)
 
 process_ec2_machines()
 
